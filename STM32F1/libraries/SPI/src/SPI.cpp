9bd7c1d311a0c5826d32150d9209f9a1fb8458fb
jjbubka <jjbubka@gmail.com>
2019-09-15 12:29:29 +0200

    fix duplicate transfer

--- STM32F1/libraries/SPI/src/SPI.cpp
+++ STM32F1/libraries/SPI/src/SPI.cpp
@@ -370,11 +370,6 @@ void SPIClass::transfer(const void *data, uint32 length)
 	write(data, length);
 }
 
-void SPIClass::transfer(const void *data, uint32 length)
-{
-	write(data, length);
-}
-
 uint8 SPIClass::transfer(uint8 byte) const
 {
     spi_dev * spi_d = _currentSetting->spi_d;
